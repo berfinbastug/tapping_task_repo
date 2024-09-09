898fdb2964b0df4fe004ae69b5fe36ae9a75cfe6
Berfin Bastug <bastugb@ESI-wsPOE001l.ESI.local>
2024-09-09 16:13:40 +0200

    correct the gitignore

--- experiment_params.py
+++ experiment_params.py
@@ -25,8 +25,8 @@ output_data_columns = ['participant_id', 'time', 'block_idx',
 #=====================
 #SET UP AUDIO PARAMETERS
 #=====================
-# device_name = 'US-4x4HR: USB Audio (hw:0,0)'
+device_name = 'US-4x4HR'
 # device_name = 'MacBook Pro Speakers'
-device_name = 'US-2x2HR'
+#device_name = 'US-2x2HR'
 device_id = 1
 stimulus_rms = 1
